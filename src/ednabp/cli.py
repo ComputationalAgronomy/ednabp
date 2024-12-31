121e0c6cca976ffbf2dde49c0ed5a3e485a360a5
Shawn <shawn.for.office@gmail.com>
2024-12-31 12:18:36 +0800

    1. Refactor the main function in the CLI module by storing argument variables (e.g., flag, help, etc.) in a dictionary, and then using a for loop to update the parser. 2. Add a custom help formatter (MyFormatter) for the parser to avoid displaying ALLCAPS in the argparse help.

--- src/ednabp/cli.py
+++ src/ednabp/cli.py
@@ -5,77 +5,101 @@ import argparse
 
 from .fastq_processor.run_processor import FastqProcessor
 
+
 def main():
+    argument_groups = {
+        "Required Input & Output Path": [
+            {"args": ["-i", "--input-path"], "kwargs": {"required": True, "help": "Input FASTA sequence file path or folder path."}},
+            {"args": ["-o", "--output-path"], "kwargs": {"required": True, "help": "Directory where output files will be saved."}}
+        ],
+        "Stages Executed": [
+            {"args": ["-e", "--enabled-stages"], "kwargs": {"nargs": "+", "help": "Process stages to be executed in order (default [run all stages]: decompress merge cutprimer fqtofa dereplicate denoise assigntaxa)."}}
+        ],
+        "Directory Names": [
+            {"args": ["--decompress-dir-name"], "kwargs": {"help": "Name of the subdirectory for the decompression stage (default: decompress)."}},
+            {"args": ["--merge-dir-name"], "kwargs": {"help": "Name of the subdirectory for the merge stage (default: merge)."}},
+            {"args": ["--cutprimer-dir-name"], "kwargs": {"help": "Name of the subdirectory for the cut-primer stage (default: cutprimer)."}},
+            {"args": ["--fqtofa-dir-name"], "kwargs": {"help": "Name of the subdirectory for FastQ to FastA conversion (default: fqtofa)."}},
+            {"args": ["--dereplicate-dir-name"], "kwargs": {"help": "Name of the subdirectory for the dereplication stage (default: dereplicate)."}},
+            {"args": ["--denoise-dir-name"], "kwargs": {"help": "Name of the subdirectory for the denoising stage (default: denoise)."}},
+            {"args": ["--assigntaxa-dir-name"], "kwargs": {"help": "Name of the subdirectory for taxonomic assignment (default: assigntaxa)."}}
+        ],
+        "File Suffixes": [
+            {"args": ["--raw-suffix"], "kwargs": {"help": "File suffix for raw input sequences (default: _R1.fastq.gz)."}},
+            {"args": ["--decompress-suffix"], "kwargs": {"help": "File suffix for decompressed sequences (default: _R1.fastq)."}},
+            {"args": ["--merge-suffix"], "kwargs": {"help": "File suffix for merged sequences (default: _merged.fastq)."}},
+            {"args": ["--cutprimer-suffix"], "kwargs": {"help": "File suffix for trimmed sequences (default: _trimmed.fastq)."}},
+            {"args": ["--dereplicate-suffix"], "kwargs": {"help": "File suffix for dereplicated sequences (default: _uniqs.fasta)."}},
+            {"args": ["--denoise-suffix"], "kwargs": {"help": "File suffix for denoised sequences (default: _zotus.fasta)."}},
+            {"args": ["--assigntaxa-suffix"], "kwargs": {"help": "File suffix for taxonomic assignment results (default: _taxa.csv)."}}
+        ],
+        "Paired-end Merge Settings": [
+            {"args": ["--maxdiff"], "kwargs": {"type": int, "help": "Maximum number of mismatches in alignment during merging (default: 5)."}},
+            {"args": ["--pctid"], "kwargs": {"type": int, "help": "Minimum percentage identity required for merging reads (default: 90)."}}
+        ],
+        "Primer Trimming Settings": [
+            {"args": ["--rm-p-5"], "kwargs": {"help": "Non-internal 5’ primer (default [MiFish-UF]: GTCGGTAAAACTCGTGCCAGC)."}},
+            {"args": ["--rm-p-3"], "kwargs": {"help": "Non-internal 3’ primer (default [rev-com MiFish-UR]: CAAACTGGGATTAGATACCCCACTATG)."}},
+            {"args": ["--error-rate"], "kwargs": {"type": float, "help": "Maximum allowable error rate for primer matching (default: 0.15)."}},
+            {"args": ["--min-read-len"], "kwargs": {"type": int, "help": "Minimum length of processed reads (default: 204)."}},
+            {"args": ["--max-read-len"], "kwargs": {"type": int, "help": "Maximum length of processed reads (default: 254)."}}
+        ],
+        "Denoising Settings": [
+            {"args": ["--minsize"], "kwargs": {"type": int, "help": "Minimum abundance of sequences to retain during denoising (default: 8)."}},
+            {"args": ["--alpha"], "kwargs": {"type": int, "help": "Denoising sensitivity parameter (default: 2)."}}
+        ],
+        "Taxonomic Assignment Settings": [
+            {"args": ["-db", "--db-path"], "kwargs": {"help": "Path to the taxonomic database (default: None)."}},
+            {"args": ["-ln", "--lineage-path"], "kwargs": {"help": "Path to the taxonomic lineage file (default: None)."}},
+            {"args": ["--evalue"], "kwargs": {"type": float, "help": "Expectation value (E) threshold for saving hits (default: 0.00001)."}},
+            {"args": ["--qcov-hsp-perc"], "kwargs": {"type": int, "help": "Percentage of query coverage for high-scoring segment pairs (default: 90)."}},
+            {"args": ["--perc-identity"], "kwargs": {"type": int, "help": "Minimum Percentage identity for taxonomic assignment (default: 90)."}},
+            {"args": ["--specifiers"], "kwargs": {"help": "Custom format specifiers for BLAST results. (default: 'qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore')."}}
+        ],
+        "Configuration Settings": [
+            {"args": ["--verbose"], "kwargs": {"action": "store_true", "help": "Enable detailed logging output (default: True)."}},
+            {"args": ["--n-cpu"], "kwargs": {"type": int, "help": "Number of CPU cores to use for processing (default: 1)."}}
+        ]
+    }
+
     parser = argparse.ArgumentParser(
-        # formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        argument_default=argparse.SUPPRESS,
-        description="A pipeline for processing eDNA bioinformatics workflows."
+        formatter_class = lambda prog: MyFormatter(prog, max_help_position=32),
+        description = "A pipeline for processing eDNA bioinformatics workflows."
     )
-    
-    # Required Input & Output Path
-    io_group = parser.add_argument_group("Required Input & Output Path")
-    io_group.add_argument("-i", "--input-path", required=True, help="Input FASTA sequence file path or directory.")
-    io_group.add_argument("-o", "--output-path", required=True, help="Directory where output files will be saved.")
-    
-    # Stages Executed
-    stage_group = parser.add_argument_group("Stages Executed")
-    stage_group.add_argument("-e", "--enabled-stages", nargs="+", help="Process stages to be executed in order (default [run all stages]: 'decompress merge cutprimer fqtofa dereplicate denoise assigntaxa').")
-    
-    # Directory Names
-    dir_group = parser.add_argument_group("Directory Names")
-    dir_group.add_argument("--decompress-dir-name", help="Name of the subdirectory for the decompression stage (default: 'decompress').")
-    dir_group.add_argument("--merge-dir-name", help="Name of the subdirectory for the merge stage (default: 'merge').")
-    dir_group.add_argument("--cutprimer-dir-name", help="Name of the subdirectory for the cut-primer stage (default: 'cutprimer').")
-    dir_group.add_argument("--fqtofa-dir-name", help="Name of the subdirectory for FastQ to FastA conversion (default: 'fqtofa').")
-    dir_group.add_argument("--dereplicate-dir-name", help="Name of the subdirectory for the dereplication stage (default: 'dereplicate').")
-    dir_group.add_argument("--denoise-dir-name", help="Name of the subdirectory for the denoising stage (default: 'denoise').")
-    dir_group.add_argument("--assigntaxa-dir-name", help="Name of the subdirectory for taxonomic assignment (default: 'assigntaxa').")
-    
-    # File Suffixes
-    suffix_group = parser.add_argument_group("File Suffixes")
-    suffix_group.add_argument("--raw-suffix", help="File suffix for raw input sequences (default: '_R1.fastq.gz').")
-    suffix_group.add_argument("--decompress-suffix", help="File suffix for decompressed sequences (default: '_R1.fastq').")
-    suffix_group.add_argument("--merge-suffix", help="File suffix for merged sequences (default: '_merged.fastq').")
-    suffix_group.add_argument("--cutprimer-suffix", help="File suffix for trimmed sequences (default: '_trimmed.fastq').")
-    suffix_group.add_argument("--dereplicate-suffix", help="File suffix for dereplicated sequences (default: '_uniqs.fasta').")
-    suffix_group.add_argument("--denoise-suffix", help="File suffix for denoised sequences (default: '_zotus.fasta').")
-    suffix_group.add_argument("--assigntaxa-suffix", help="File suffix for taxonomic assignment results (default: '_taxa.csv').")
-    
-    # Merge Settings
-    merge_group = parser.add_argument_group("Paired-end Merge Settings")
-    merge_group.add_argument("--maxdiff", type=int, help="Maximum number of mismatches in alignment during merging (default: 5).")
-    merge_group.add_argument("--pctid", type=int, help="Minimum percentage identity required for merging reads (default: 90).")
-    
-    # Primer Cutting Settings
-    cuter_group = parser.add_argument_group("Primer Trimming Settings")
-    cuter_group.add_argument("--rm-p-5", help="Non-internal 5’ primer (default: 'GTCGGTAAAACTCGTGCCAGC').")
-    cuter_group.add_argument("--rm-p-3", help="Non-internal 3’ primer (default: 'CAAACTGGGATTAGATACCCCACTATG').")
-    cuter_group.add_argument("--error-rate", type=float, help="Maximum allowable error rate for primer matching (default: 0.15).")
-    cuter_group.add_argument("--min-read-len", type=int, help="Minimum length of processed reads (default: 204).")
-    cuter_group.add_argument("--max-read-len", type=int, help="Maximum length of processed reads (default: 254).")
-    
-    # Denoising Settings
-    denoise_group = parser.add_argument_group("Denoising Settings")
-    denoise_group.add_argument("--minsize", type=int, help="Minimum abundance of sequences to retain during denoising (default: 8).")
-    denoise_group.add_argument("--alpha", type=int, help="Denoising sensitivity parameter (default: 2).")
-    
-    # Taxonomic Assignment Settings
-    taxa_group = parser.add_argument_group("Taxonomic Assignment Settings")
-    taxa_group.add_argument("-db", "--db-path", help="Path to the taxonomic database (default: None).")
-    taxa_group.add_argument("-lng", "--lineage-path", help="Path to the taxonomic lineage file (default: None).")
-    taxa_group.add_argument("--evalue", type=float, help="Expectation value (E) threshold for saving hits (default: 0.00001).")
-    taxa_group.add_argument("--qcov-hsp-perc", type=int, help="Percentage of query coverage for high-scoring segment pairs (default: 90).")
-    taxa_group.add_argument("--perc-identity", type=int, help="Minimum percentage identity for taxonomic assignment (default: 90).")
-    taxa_group.add_argument("--specifiers", help="Custom format specifiers for BLAST results. (default: 'qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore').")
-    
-    # Configuration Settings
-    config_group = parser.add_argument_group("Configuration Settings")
-    config_group.add_argument("--verbose", action="store_true", help="Enable detailed logging output (default: True).")
-    config_group.add_argument("--n-cpu", type=int, help="Number of CPU cores to use for processing (default: 1).")
+
+    for group_name, args_list in argument_groups.items():
+        group = parser.add_argument_group(group_name)
+        for arg_data in args_list:
+            group.add_argument(*arg_data["args"], **arg_data["kwargs"])
 
     options = vars(parser.parse_args())
 
     FastqProcessor(**options)
+
+class MyFormatter(argparse.HelpFormatter):
+    """
+        for matt wilkie on SO
+        https://stackoverflow.com/questions/9642692/argparse-help-without-duplicate-allcaps
+    """
+    def _format_action_invocation(self, action):
+        if not action.option_strings:
+            default = self._get_default_metavar_for_positional(action)
+            metavar, = self._metavar_formatter(action, default)(1)
+            return metavar
+        else:
+            parts = []
+            # if the Optional doesn't take a value, format is:
+            #    -s, --long
+            if action.nargs == 0:
+                parts.extend(action.option_strings)
+            # if the Optional takes a value, format is:
+            #    -s ARGS, --long ARGS
+            else:
+                args_string = self._format_args(action, '')
+                for option_string in action.option_strings:
+                    parts.append(option_string)
+                return '%s %s' % (', '.join(parts), args_string)
+            return ', '.join(parts)
 
 if __name__ == "__main__":
     main()

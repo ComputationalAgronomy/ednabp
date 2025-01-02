c1c12768a2edfe473da0ae78a92a33c04f0a6dda
Shawn <shawn.for.office@gmail.com>
2025-01-03 00:12:53 +0800

    Refactor diversity metrics ploting related modules and create the main module run_dm_analysis for all DM related works

---
+++ src/ednabp/analysis_toolkit/run_dm_analysis.py
@@ -0,0 +1,15 @@
+from .runner_exec.dm_barchart_plotter import BarchartPlotter
+from .runner_exec.dm_contourf_plotter import ContourPlotter
+from .runner_exec.dm_heatmap_plotter import HeatmapPlotter
+from .runner_exec.dm_table_writer import DMWriter
+
+class DMAnalyser(BarchartPlotter, ContourPlotter, HeatmapPlotter, DMWriter):
+    """
+    A class that inherits from ContourPlotter, BarchartPlotter, HeatmapPlotter, and DMWriter.
+    This class is used for diversity metrics related analysis and visualization.
+    """
+    def __init__(self, sampledata = None, no_verbose: bool = False):
+        if sampledata is not None:
+            DMWriter.__init__(self, sampledata, no_verbose)
+        else:
+            BarchartPlotter.__init__(self, no_verbose)

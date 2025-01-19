ee6c7535d2b6958b3b4f137e72f9c2647b106856
Shawn <shawn.for.office@gmail.com>
2025-01-19 14:34:30 +0800

    minor changes for lazy import

--- src/ednabp/__init__.py
+++ src/ednabp/__init__.py
@@ -1,6 +1,6 @@
-from .fastq_processor.run_processor import FastqProcessor
+from ednabp.fastq_processor.run_processor import FastqProcessor
+from ednabp.analysis_toolkit.runner_exec.data_container import SampleData
+from ednabp.analysis_toolkit.run_dm_analysis import DMAnalyser
+from ednabp.analysis_toolkit.run_seq_analysis import SeqAnalyser
 
-from .analysis_toolkit.run_dm_analysis import DMAnalyser
-from .analysis_toolkit.run_seq_analysis import SeqAnalyser
-
-__all__ = ["FastqProcessor", "DMAnalyser", "SeqAnalyser"]
+__all__ = ["FastqProcessor", "SampleData", "DMAnalyser", "SeqAnalyser"]
